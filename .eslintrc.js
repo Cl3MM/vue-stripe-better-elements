1a084da1b2785f056365d8e15a86f8f799418e90
clement <clement@antidots.com>
2018-11-16 14:33:38 +0100

    add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // "eslint:recommended",
+    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended"
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  parser: "vue-eslint-parser"
+}
