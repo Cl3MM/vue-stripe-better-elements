7edead4650eb4f63a7c793a1a81db68c2a2e5f21
clement <clement@antidots.com>
2019-09-18 17:03:57 +0200

    chore: lint

--- src/StripeElement.vue
+++ src/StripeElement.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <slot
-      :element="element"
-      :elements="elements"
-    />
+    <slot :element="element" :elements="elements" />
   </div>
 </template>
 
 <script>
-import props from './props'
-import { create, destroy } from './stripeElements'
+import props from "./props";
+import { create, destroy } from "./stripeElements";
 
 export default {
   props,
@@ -23,30 +20,40 @@ export default {
       elOptions: this.elOptions
     });
 
-    this.element.on('blur', () => this.$emit('blur'))
-    this.element.on('focus', () => this.$emit('focus'))
-    this.element.on('change', event => this.$emit('change', event))
+    this.element = this.elements.element;
+
+    this.element.on("blur", () => this.$emit("blur"));
+    this.element.on("focus", () => this.$emit("focus"));
+    this.element.on("change", event => this.$emit("change", event));
   },
 
-  beforeDestroy () {
-    this.element.unmount()
-    this.element.destroy()
-    destroy(this.type, this.stripe)
+  beforeDestroy() {
+    this.element.unmount();
+    this.element.destroy();
+    destroy(this.type, this.stripe);
   },
 
-  mounted () {
+  mounted() {
     // Vue likes to stay in control of $el but Stripe needs a real element
-    const el = document.createElement('div')
-    this.element.mount(el)
+    const el = document.createElement("div");
+    this.element.mount(el);
     // this.$children cannot be used because it expects a VNode :(
-    this.$el.prepend(el)
+    this.$el.prepend(el);
   },
 
   methods: {
-    blur () { this.element.blur() },
-    clear () { this.element.clear() },
-    focus () { this.element.focus() },
-    update (opts) { this.element.update(opts) }
+    blur() {
+      this.element.blur();
+    },
+    clear() {
+      this.element.clear();
+    },
+    focus() {
+      this.element.focus();
+    },
+    update(opts) {
+      this.element.update(opts);
+    }
   }
-}
+};
 </script>
