13a532db538531ac0fc9fc3facb82a7f527240b3
clement <clement@antidots.com>
2019-09-18 16:58:34 +0200

    chore: use ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-import "./lib"
-import { Stripe, baseStyle } from "./stripeElements"
-import StripeElement from "./StripeElement"
+import "./lib";
+import { Stripe, baseStyle } from "./stripeElements";
+import StripeElement from "./StripeElement";
 
-module.exports = {
+export { Stripe, baseStyle } from "./stripeElements";
+export { default as StripeElement } from "./StripeElement";
+
+export const instance = Stripe.instance;
+export const components = Stripe.components;
+export const get = Stripe.get;
+
+export default {
   StripeElement,
   baseStyle,
   get Stripe() {
-    return Stripe
+    return Stripe;
   },
   get instance() {
-    return Stripe.instance
+    return Stripe.instance;
   },
   get components() {
-    return Stripe.components
+    return Stripe.components;
   },
   get get() {
-    return Stripe.get
+    return Stripe.get;
   }
-}
+};
